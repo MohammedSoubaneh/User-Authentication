c93dc79fb73c54489e5decf62027726cf0b9da1f
MohammedSoubaneh <soubcompany@gmail.com>
2022-10-02 08:35:58 -0600

    Post Model

--- Blog/posts/models.py
+++ Blog/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-    
+class Post(models.Model):
+
+    STATUS_CHOICE = (
+        ('DRAFT', 'Draft'),
+        ('PUBLISHED', 'Published')
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
+    title = models.CharField(max_length=50)
+    text_field = models.TextField()
+    status = models.CharField(choice=STATUS_CHOICE)
